18576d9d0185d3812ccfad1ff03fbfd786d193a6
bogda <bogdanhalici1989@gmail.com>
2022-10-27 18:15:39 +0100

    1st commit

---
+++ src/Exercitiu.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exercitiu {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> nrs = new ArrayList<>();
+       // Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+
+            nrs.add(1);
+            nrs.add(2);
+            nrs.add(3);
+            nrs.add(4);
+            nrs.add(2);
+            nrs.add(9);
+            nrs.add(4);
+            nrs.add(6);
+            nrs.add(3);
+            nrs.add(1);
+
+
+           // nrs.add(scanner.nextInt());
+        }
+        boolean suntemInCrestere = false;
+        int pozitiaDeInceput = 0;
+        int max = 0;
+        for (int i = 0; i < nrs.size()-1; i++) {
+            if (nrs.get(i) < nrs.get(i + 1)) {
+                if (!suntemInCrestere) {
+                    pozitiaDeInceput = i;
+                }
+                suntemInCrestere = true;
+            } else {
+                suntemInCrestere = false;
+                int lungimeaSirului = i - pozitiaDeInceput +1;
+                if (max < lungimeaSirului) {
+                    max = lungimeaSirului;
+                }
+            }
+        }
+        System.out.println(max);
+    }
+}
